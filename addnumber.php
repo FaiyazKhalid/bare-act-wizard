a242a6f34227af9f9001133f4ea8b3bd15fc2d0c
FaiyazKhalid <fkhalid42@gmail.com>
2021-10-25 20:34:42 +0530

    Create addnumber.php

---
+++ addnumber.php
@@ -0,0 +1,17 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<style>
+    .form-control {
+    display: block;
+    width: 44%;
+    }
+</style>
+
+<form method="post" class="form-control" action="bareact-box.php">
+        <center>
+        <strong>How many sections are there in this bare act?</strong><br> 
+        <input type="number" class="form-control" name="number"><br><br/>
+
+        <br/>
+        <input type="submit" class='btn btn-primary ladda-button' value="submit"  >
+    </form>
+ </center>
